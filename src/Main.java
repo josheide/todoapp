43d6f821ba398bf7b2f90241005df4827d2afeff
Tamas Csillag <tamas.csillag@nyu.edu>
2023-04-22 14:06:39 +0200

    v102
    If you properly exit the program the items on your to do list will be saved to a pre-defined .txt file.
    When you run the program again you can list the saved items.
    When completing a task you get a confirmation message.
    If you already completed the task and you try to complete it again you are told that you already completed that task.

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,62 @@
 // then press Enter. You can now see whitespace characters in your code.
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * For my array to take two value, a string and a boolean I created a class 'Task'
+ * Task class has a constructor method, which takes two arguments: name and isComplete.
+ */
+
+class Task {
+    String name;
+    boolean isComplete;
+
+    Task(String name, boolean isComplete) {
+        this.name = name;
+        this.isComplete = isComplete;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
 
-        /**
-         * For my array to take two value, a string and a boolean I created a class
-         * Would a dictionary work?
-         */
-        class Task {
-            String name;
-            boolean isComplete;
+        String filePath = "C:/Users/tc110/Dropbox/Prog/todoapp/todolist.txt";
+        File todolist = new File("C:/Users/tc110/Dropbox/Prog/todoapp/todolist.txt");
 
-            // The Task class also has a constructor method, which takes two arguments: name and isComplete.
+        ArrayList<Task> taskList = new ArrayList<Task>();
 
-            Task(String name, boolean isComplete) {
-                this.name = name;
-                this.isComplete = isComplete;
+        try (BufferedReader reader = new BufferedReader(new FileReader(todolist))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] taskData = line.split(" - ");
+                taskList.add(new Task(taskData[0], Boolean.parseBoolean(taskData[1])));
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        ArrayList<Task> inputList = new ArrayList<Task>();
 
         String userInput = " ";
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("In this app you can do three things!");
-        System.out.println("- You can add tasks to your to do list, type: 'add' ");
-        System.out.println("- You can remove items from your to do list, type: 'delete' ");
-        System.out.println("- Or you can ask the program to show your to do list, type: 'list' ");
+        System.out.println("In this app you can manage your to-do list! Here's how it works:");
+        System.out.println(" ");
+        System.out.println("- You can add tasks to your to do list by typing: 'add' ");
+        System.out.println("- You can remove items from your to do list by typing: 'delete' ");
+        System.out.println("- You  can ask the program to show your to do list by typing: 'list' ");
         System.out.println("- You can complete a task by typing 'complete' ");
         System.out.println("");
 
         while (!userInput.equals("exit")) {
 
+            System.out.println(" ");
             System.out.println("Please type what you would like to do now: ");
-            System.out.println("If you would like to exit the app, type: 'exit'");
+            System.out.println("If you would like to save your tasks and exit the app, type: 'exit'");
 
             userInput = scanner.nextLine();
 
@@ -46,22 +65,19 @@ public class Main {
 
                 System.out.println("Please type the task that you would like to add!");
                 String userTask = scanner.nextLine();
-                inputList.add (new Task(userTask, false));
+                taskList.add (new Task(userTask, false));
                 System.out.println (userTask + " has been added!");
-
             }
 
             else if (userInput.equals("delete")) {
 
                 System.out.println("Please enter the name of the task you would like to remove:");
-
                 String userTask = scanner.nextLine();
 
                 boolean foundTask = false;
-
-                for (Task task : inputList) {
+                for (Task task : taskList) {
                     if (task.name.equals(userTask)) {
-                        inputList.remove(task);
+                        taskList.remove(task);
                         foundTask = true;
                         System.out.println(userTask + " has been removed from the to-do list.");
                         break;
@@ -77,21 +93,25 @@ public class Main {
                 String userTask = scanner.nextLine();
 
                 boolean foundTask = false;
-
-                for (Task task : inputList) {
-                    if (task.name.equals(userTask)) {
+                for (Task task : taskList) {
+                    if (task.name.equals(userTask) && task.isComplete == false) {
                         task.isComplete = true;
                         foundTask = true;
+                        System.out.println("Congratulations, you completed: " +userTask);
                         break;
                     }
-                }
+                    else if (task.name.equals(userTask) && task.isComplete) {
+                        System.out.println("You already completed " +userTask);
+                    }
+
+                    }
                 if (!foundTask) {
-                    System.out.println("Task not found.");
+                    System.out.println(userTask + " is currently not on the to-do list.");
                 }
             }
 
             else if (userInput.equals("list")) {
-                for (Task element : inputList) {
+                for (Task element : taskList) {
                     if(element.isComplete){
                         System.out.println(element.name + " - is complete");
                     }
@@ -102,6 +122,19 @@ public class Main {
             }
 
             else if (userInput.equals("exit")) {
+
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("C:/Users/tc110/Dropbox/Prog/todoapp/todolist.txt"));
+                    for (Task task : taskList) {
+                        writer.write(task.name + " - " + task.isComplete + "\n");
+                    }
+                    writer.close();
+                    System.out.println("Tasks written to file.");
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
                 userInput = "exit";
             }
 
